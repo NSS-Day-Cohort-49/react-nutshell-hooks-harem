4f6af3513e6a498aa131f8e60d800fdc891cf25a
Matt <Mattxiong_14@hotmail.com>
2021-07-19 14:06:09 -0500

    finished message provider

--- src/components/messages/MessageProvider.js
+++ src/components/messages/MessageProvider.js
@@ -6,13 +6,13 @@ export const MessageProvider = (props) => {
     const [messages, setMessages] = useState([])
 
     const getMessages = () => {
-        return fetch(`http://localhost:8088/messages`)
+        return fetch("http://localhost:8088/messages?_expand=user")
         .then(res => res.json())
         .then(setMessages)
     }
 
-    const addMessage = message => {
-        return fetch(`http://localhost:8088/messages`, {
+    const addMessage = (message) => {
+        return fetch("http://localhost:8088/messages", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -23,17 +23,33 @@ export const MessageProvider = (props) => {
         .then(getMessages)
 }
 
-    const deleteMessage = id => {
-        return fetch(`http://localhost:8088/messages/${id}`, {
+    const editMessage = (message) => {
+        return fetch(`http://localhost:8088/messages/${message.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: json.stringify(message)
+        })
+            .then(getMessages)
+    }
+
+    const deleteMessage = (messageId) => {
+        return fetch(`http://localhost:8088/messages/${messageId}`, {
             method: "DELETE"
         })
     
         .then(getMessages)
-}
+    }
+
+    const getMessageById = (id) => {
+        return fetch (`http://localhost:8088/messages/${id}?_expand=user`)
+        .then(res => res.json())
+    }
 
     return (
         <MessageContext.Provider value={{
-            messages, getMessages, addMessage, deleteMessage
+            messages, getMessages, addMessage, deleteMessage, getMessageById, editMessage
         }}>
 
             {props.childern}
