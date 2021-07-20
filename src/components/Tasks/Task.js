62e59088d03c7e6d4ebdbda8dd91ac42c3fe2d13
IanJames2 <ianjms81@gmail.com>
2021-07-19 22:32:52 -0500

    Added all the files necessary to push the tasks create and read code to gh

---
+++ src/components/Tasks/Task.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Task.css"
+export const TaskCard = ({ task }) => (
+    <section className="task">
+    <h3 className="task__name">Task Name: {task.taskName}</h3>
+      <div className="task__creator">Tasked By: {task.user.name}</div>
+      <div className="task__expectedCompletionDate">Expected Completion Date: {task.expectedCompletionDate}</div>
+      <div className="task__dateCompleted">Task Completion Date: {task.dateCompleted}</div>
+    </section>
+  )
---
+++ src/components/Tasks/TaskList.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect } from "react";
+import { TaskContext } from "./TaskProvider";
+import { TaskCard } from "./TaskCard";
+import { useHistory } from 'react-router-dom';
+import "./Task.css";
+export const TaskList = () => {
+    const { tasks, getTasks,  } = useContext(TaskContext)
+    const history = useHistory()
+    useEffect(() => {
+        console.log("TaskList: UseEffect is being called properly in the browser")
+        getTasks()
+    }, [])
+    return (
+  <>
+        <h2>List of Tasks</h2>
+        <button
+          className="clickMe"
+          onClick={() => {
+            history.push("/tasks_add");
+          }}
+        >
+          Add A Task
+        </button>
+        <div className="tasks">
+          {tasks.map((task) => {
+            return (<TaskCard key={task.id} task={task} />
+          );
+        })}
+        </div>
+  </>
+    );
+  }
